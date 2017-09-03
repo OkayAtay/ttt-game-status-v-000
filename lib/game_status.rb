1bd560b49989ebde98b309803276556b771a9540
Araba Taylor <araba.taylor20@gmail.com>
2017-09-03 22:45:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,10 +15,17 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
- if WIN_COMBINATIONS[0].select do |win_index|
-    position_taken?(board, win_index)
-        else
-          return false
+  WIN_COMBINATIONS.each do |win_array|
+    if win_array.all? { |win_index| position_taken?(board, win_index) }
+      win_index_1 = win_array[0]
+      win_index_2 = win_array[1]
+      win_index_3 = win_array[2]
+
+      if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
+        return win_array
+      else
+      end
     end
+    return false
   end
 end
