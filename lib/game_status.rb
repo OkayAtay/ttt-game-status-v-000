20cad870902bb6b58e4da1fb1c03f4f505da4a5f
Araba Taylor <araba.taylor20@gmail.com>
2017-09-03 22:43:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,6 @@ WIN_COMBINATIONS = [
 def won?(board)
  if WIN_COMBINATIONS[0].select do |win_index|
     position_taken?(board, win_index)
-    win_index_1 = win_array[0]
-     win_index_2 = win_array[1]
-     win_index_3 = win_array[2]
   end
   if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
           return win_array
